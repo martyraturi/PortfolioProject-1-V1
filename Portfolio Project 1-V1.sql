e98639e5fd3ab2e9b5851c49e211a6a04abfaeb3
martyraturi <64448284+martyraturi@users.noreply.github.com>
2021-09-05 16:38:23 +1200

    Update Portfolio Project 1-V1.sql

--- Portfolio Project 1-V1.sql
+++ Portfolio Project 1-V1.sql
@@ -184,3 +184,23 @@ WHERE dea.continent IS NOT NULL
 --ORDER BY 2,3
 SELECT * , (RollingPeopleVaccinated/ population) * 100
 FROM #PercentPopulationVaccinated
+
+--Creating View to store data for later visualizations
+
+
+
+CREATE View PercentPopulationVaccinated AS
+SELECT 
+	dea.continent,dea.location, dea.date , dea.population, 
+	vac.new_vaccinations, SUM(Convert(int, vac.new_vaccinations)) 
+	OVER (PARTITION BY  dea.location ORDER BY dea.location, dea.date) 
+	AS RollingPeopleVaccinated
+FROM Test..CovidDeaths dea
+JOIN Test..CovidVaccinations vac
+	ON  dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3
+
+SELECT *
+FROM PercentPopulationVaccinated
